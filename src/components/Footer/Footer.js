3f27d0a8ff64a09b11644fd816810d0edabb10fe
Keshav-babu <keshavbabu742@gmail.com>
2022-05-30 10:29:06 +0530

    Quiz Application in React.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+
+
+
+const Footer=()=>{
+    return (
+        <div style={{textAlign: "center",marginBottom: 10,}}>
+            {/* Made with Love */}
+            Tech Stacks :React & MaterialUI
+        </div>
+    )
+}
+
+export default Footer;
